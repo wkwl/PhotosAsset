76b7a3b5be20c37504173bc5e72d14f7518884b2
wkwl <wkw0913@163.com>
2017-07-06 15:40:32 +0800

    照片选择器

---
+++ PhotosAsset/PhotosAsset/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotosAsset
+//
+//  Created by 77 on 2017/7/6.
+//  Copyright © 2017年 77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotosAsset/PhotosAsset/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotosAsset
+//
+//  Created by 77 on 2017/7/6.
+//  Copyright © 2017年 77. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
